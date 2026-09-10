93391adeb03683f756d624b04944c41bf413b416
MYNAMEISJEFF482 <45806318+caden4314@users.noreply.github.com>
2026-09-10 10:56:54 -0500

    Add display library public API

---
+++ frontend/src/display/index.js
@@ -0,0 +1,5 @@
+export {createDisplayRuntime,clamp,lerp} from './engine.js';
+export {normalizeValues,sampleWave,drawDotMatrix,drawPixelGrid,drawWave,drawGrid,drawPolyline} from './primitives.js';
+export {smoothstep,smootherstep,pulse,triangle,springStep,exponentialSmoothing,withGlow,drawScanlines,drawVignette,flashEnvelope} from './effects.js';
+export {default as DisplayCanvas} from './DisplayCanvas.jsx';
+export {default as LambdaSurface} from './LambdaSurface.jsx';
